0264f9b54aae05353cfad4ef9ff3624f01c52c2f
Volodymyr Lench <volodymyr.lench@binary-studio.com>
2018-02-24 18:02:18 +0200

    add base templates for users

---
+++ src/components/UserItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-sm-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">{{ user.name }}</div>
+            <p class="card-text">{{ user.email }}</p>
+            <a class="btn btn-outline-danger" @click="deleteUser(user)">Delete</a>
+            <a class="btn btn-primary" @click="editUser(user, user)">Edit</a>
+        </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        }
+    },
+
+    methods: {
+        deleteUser(user) {
+            this.$emit('deleteUser', user);
+        },
+
+        editUser(user, data) {
+            this.$emit('editUser', user, data);
+        }
+    }
+}
+</script>
---
+++ src/components/UserList.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <user-item v-for="user in users" 
+                :key="user.id"
+                :user="user"
+                @deleteUser="deleteUser"
+                @editUser="editUser"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import UserItem from './UserItem.vue';
+
+export default {
+    props: {
+        users: {
+            type: Array,
+            required: true
+        }
+    },
+
+    components: {
+        UserItem
+    },
+
+    methods: {
+        deleteUser(user) {
+            const i = this.users.indexOf(user);
+
+            if (i !== -1) {
+                this.users.splice(i, 1);
+            }
+        },
+
+        editUser(user, data) {
+
+        }
+    }
+}
+</script>
